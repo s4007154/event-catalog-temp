d182eb1ea5ca2c91a92f4a048820d53626eb3e68
s4007154 <s4007154@student.rmit.edu.au>
2025-04-09 20:05:40 +1000

    second change

---
+++ my-catalog/eventCatalogGeneration.js
@@ -0,0 +1,35 @@
+import utils from '@eventCatalog/sdk';
+
+const {writeDomain, writeServiceToDomain, writeEventToService, addServiceToDomain, addEventToService} = utils('my-catalog');
+
+await writeDomain({
+    id: "ExampleDomain",
+    name: "ExampleDomain",
+    version: '0.0.1',
+    owners: '',
+    badges: '',
+    markdown: '',},
+    {override: true}) 
+
+await writeServiceToDomain({
+     id: "ExampleService",
+     name: "ExampleService",
+     version: '0.0.1',
+     owners: '',
+     badges: '',
+     markdown: '',},
+     {id: "ExampleDomain"},
+     {override: true}) 
+
+await addServiceToDomain("ExampleDomain", {service: "ExampleService", version: '0.0.1}'});
+
+await writeEventToService({
+    id: "ExampleEvent",
+    name: "ExampleEvent",
+    version: '0.0.1',
+    owners: '',
+    badges: '',
+    markdown: '',},
+    {id: "ExampleService"})
+
+await addEventToService("ExampleService", "receives", {event: "ExampleEvent", version: '0.0.1'});
